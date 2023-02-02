fc8878300162dba70669655b56aee64d1cb1e6af
weton01 <wellington_ferreira280@hotmail.com>
2023-02-01 21:55:33 -0300

    feat: ensure add core packages

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GridModule } from '@coreui/angular';
+
 import { IndexComponent } from './pages/index/index.component';
 import { HomeRoutingModule } from './home-routing.module';
 
@@ -9,7 +11,8 @@ import { HomeRoutingModule } from './home-routing.module';
   ],
   imports: [
     HomeRoutingModule,
-    CommonModule
-  ]
+    CommonModule,
+    GridModule
+   ]
 })
 export class HomeModule { }
--- src/app/home/pages/index/index.component.ts
+++ src/app/home/pages/index/index.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent {
+  public tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 
 }
